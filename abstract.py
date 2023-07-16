632ba574bb984b6d2ea7ddb3f26e181958243e1c
YoyoBFighter <139683102+YoyoBFighter@users.noreply.github.com>
2023-07-16 13:08:52 +0300

    Add files via upload

--- abstract.py
+++ abstract.py
@@ -1,1 +1,25 @@
+from abc import ABCMeta, abstractmethod
+
+class lang_program(metaclass=ABCMeta):
+    @abstractmethod
+    def Supports_OOP(self):
+    #  return 'Yes'
+        pass
+        
+    def say_hello(self):
+        return "hello"
+class Python(lang_program):
+    def Supports_OOP(self):
+        return 'Yes'
+    
+class C(lang_program):
+    def Supports_OOP(self):
+        return 'No'
+    
+    
+My_lang=Python()
+print(My_lang.Supports_OOP())
+
+
+    
 
